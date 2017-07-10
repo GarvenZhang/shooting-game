2061039b095f5b23c3870ce4de98a58c8ba04a4c
coverguo(郭锋棉) <coverguo@tencent.com>
2017-07-10 21:06:26 +0800

    feat: 增加game项目基础代码

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// 元素
+var container = document.getElementById('game');
+
+/**
+ * 整个游戏对象
+ */
+var GAME = {
+  /**
+   * 初始化函数,这个函数只执行一次
+   * @param  {object} opts 
+   * @return {[type]}      [description]
+   */
+  init: function(opts) {
+    this.status = 'start';
+    this.bindEvent();
+  },
+  bindEvent: function() {
+    var self = this;
+    var playBtn = document.querySelector('.js-play');
+    // 开始游戏按钮绑定
+    playBtn.onclick = function() {
+      self.play();
+    };
+  },
+  /**
+   * 更新游戏状态，分别有以下几种状态：
+   * start  游戏前
+   * playing 游戏中
+   * failed 游戏失败
+   * success 游戏成功
+   * stop 游戏暂停
+   */
+  setStatus: function(status) {
+    this.status = status;
+    container.setAttribute("data-status", status);
+  },
+  play: function() {
+    this.setStatus('playing');
+  }
+};
+
+
+// 初始化
+GAME.init();
